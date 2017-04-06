1183eead1f8e96f4b0961bc9e28c1ee9060523e5
Christopher White <cwhite01+github@gmail.com>
2017-04-06 13:53:31 +0200

    initial

---
+++ pizzaluigi/src/be/vdab/servlets/IndexServlet.java
@@ -0,0 +1,27 @@
+package be.vdab.servlets;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class IndexServlet
+ */
+@WebServlet("/index.htm")
+public class IndexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String VIEW = "/WEB-INF/JSP/index.jsp";
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int uur = LocalDateTime.now().getHour();
+		request.setAttribute("begroeting", uur >= 6 && uur < 12 ? "Goede morgen" :
+		uur >= 12 && uur < 18 ? "Goede middag": "Goede avond");
+		request.getRequestDispatcher(VIEW).forward(request, response);
+	}
+
+}
