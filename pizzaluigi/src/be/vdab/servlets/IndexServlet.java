774ca5fa736fed675f21870032db9299a6d11623
Christopher White <cwhite01+github@gmail.com>
2017-04-07 09:48:03 +0200

    Javabean

--- pizzaluigi/src/be/vdab/servlets/IndexServlet.java
+++ pizzaluigi/src/be/vdab/servlets/IndexServlet.java
@@ -1,5 +1,5 @@
 package be.vdab.servlets;
-
+import be.vdab.entities.*;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import be.vdab.entities.Begroeting;
+
 
 /**
  * Servlet implementation class IndexServlet
@@ -19,7 +19,8 @@ public class IndexServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute("begroeting", new Begroeting());
-		request.getRequestDispatcher(VIEW).forward(request, response);
+		request.setAttribute("zaakvoerder", new Persoon("Luigi", "Peperone", 7, true, new Adres("Grote markt", "3", 9700, "Oudenaarde")));
+		request.getRequestDispatcher(VIEW).forward(request, response); /* must be last */
 	}
 
 }
